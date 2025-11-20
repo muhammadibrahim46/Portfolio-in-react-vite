66dde2c453288d3239b060db37027a2f0de62fdb
Ibrahimfridi <ibrahimafridi446644@gmail.com>
2025-11-20 23:31:45 +0500

    changing in opensource

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,19 +3,19 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className="w-full bg-emerald-600/95 text-white mr-12 mb-30 py-3 fixed top-0 left-0 z-50">
+      <nav className="w-full bg-emerald-600/95 text-white mr-12 mb-30 py-2 fixed top-0 left-0 z-50">
         <div className="flex justify-center">
           
-          <h1 className="ml-10 bg-emerald-500 shadow-md rounded-full font-bold w-14 h-14 flex items-center justify-center p-3">
+          <h1 className="ml-10 bg-emerald-500 shadow-md rounded-full font-bold w-16 h-16 flex items-center justify-center p-3">
             MI
           </h1>
 
-          <ul className="flex text-lg justify-center items-center italic gap-10 ml-160">
-            <li><a href="#about" className="hover:text-red-500">About</a></li>
-            <li><a href="#skills" className="hover:text-red-500">Skills</a></li>
-            <li><a href="#services" className="hover:text-red-500">Services</a></li>
-            <li><a href="#opensource" className="hover:text-red-500">Opensource</a></li>
-            <li><a href="#contact" className="hover:text-red-500">Contact</a></li>
+          <ul className="flex text-2xl justify-center items-center italic gap-10 ml-160 ">
+            <li><a href="#about" className="hover:text-red-500 duration-400">About</a></li>
+            <li><a href="#skills" className="hover:text-red-500 duration-400 ">Skills</a></li>
+            <li><a href="#services" className="hover:text-red-500 duration-400">Services</a></li>
+            <li><a href="#opensource" className="hover:text-red-500 duration-400">Opensource</a></li>
+            <li><a href="#contact" className="hover:text-red-500 duration-400">Contact</a></li>
           </ul>
 
         </div>
--- src/components/Opensource/index.jsx
+++ src/components/Opensource/index.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+
+const opensource = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    Paragraph: "A personal portfolio website to showcase my skills, projects, and contact info using React and Tailwind CSS.",
+    icon:"https://github.com/muhammadibrahim46",
+     tech: ["React", "Redux", "Tailwind CSS"],
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    Paragraph: "A real-time weather app that fetches data from an API using React and Tailwind CSS.",
+    icon:"https://github.com/muhammadibrahim46",
+     tech: ["React", "Redux", "Tailwind CSS"],
+  },
+  {
+    id: 3,
+    title: "Todo App",
+    Paragraph: "A simple and clean todo app using React, Redux, and Tailwind CSS.",
+    icon:"https://github.com/muhammadibrahim46",
+     tech: ["React", "Redux", "Tailwind CSS"],
+  }
+];
 
 function Opensource() {
   return (
-    <div></div>
-  )
+    <section id="opensource">
+      <div className="bg-blue-200 py-20 min-w-2xs">
+        
+        {/* Heading */}
+        <div className="text-center">
+          <p className="text-sm text-emerald-600 font-medium ">What I build</p>
+          <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-800 mt-1">
+            Open Source Projects
+          </h1>
+          <p className="text-gray-600 mt-1">
+            I create open source projects that solve real problems, showcase modern technologies, <br /> and demonstrate best practices...
+          </p>
+        </div>
+
+        {/* Cards Section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10 px-4">
+          {opensource.map((item) => (
+            <div 
+              key={item.id} 
+              className="p-10 bg-white  rounded-xl shadow-md hover:shadow-lg transition"
+            >
+              <h2 className="text-xl font-semibold">{item.title}</h2>
+              <p className="text-gray-700 mt-2">{item.Paragraph}</p>
+
+              <div className="mt-4 flex flex-wrap gap-2">
+                {item.tech.map((t) => (
+                  <span 
+                    key={t} 
+                    className="text-sm bg-emerald-50 border border-green-200 text-emerald-600 px-2 py-1 rounded-full font-medium"
+                  >
+                    {t}
+                  </span>
+                  
+                ))}
+              </div>
+              <div className="flex gap-4 mt-4">
+                <a
+                  href={item.icon}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-1 text-gray-800 hover:text-emerald-500 duration-300 font-semibold"
+                >
+                  <FaGithub /> GitHub
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
-export default Opensource
+export default Opensource;
